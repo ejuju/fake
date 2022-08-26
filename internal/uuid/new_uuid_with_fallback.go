3d23c78bcf7580db4420ff157afdd7e5af722198
ejuju <juju.sel.mail@gmail.com>
2022-08-26 02:25:35 +0200

    fix uuid call

--- internal/uuid/new_uuid_with_fallback.go
+++ internal/uuid/new_uuid_with_fallback.go
@@ -5,9 +5,5 @@ import (
 )
 
 func NewUUIDWithFallback() string {
-	id, err := uuid.NewUUID()
-	if err != nil {
-		return "4631c17f-5f66-42f7-bb90-39b2619b8b44" // fallback uuid
-	}
-	return id.String()
+	return uuid.New().String()
 }
